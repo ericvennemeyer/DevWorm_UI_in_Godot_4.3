433677968e51301f26b315c997cba78c4c3c0a09
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-13 16:47:54 -0700

    Tutorial: HowTo Menu

--- popup_menu_manager.gd
+++ popup_menu_manager.gd
@@ -2,6 +2,7 @@ extends MarginContainer
 
 @export var menu_screen: VBoxContainer
 @export var open_menu_screen: VBoxContainer
+@export var help_menu_screen: MarginContainer
 
 
 func toggle_visibility(object: Control) -> void:
@@ -11,3 +12,8 @@ func toggle_visibility(object: Control) -> void:
 func _on_toggle_menu_button_pressed() -> void:
 	toggle_visibility(menu_screen)
 	toggle_visibility(open_menu_screen)
+
+
+func _on_toggle_help_menu_button_pressed() -> void:
+	toggle_visibility(help_menu_screen)
+	toggle_visibility(menu_screen)
